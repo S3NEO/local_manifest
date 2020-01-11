761b7e1bfd9e8719b581c64242ae5a34f520ae5b
RomanDesigner <romashkarus08@gmail.com>
2020-01-11 20:13:04 +0300

    Use script

---
+++ README.md
@@ -0,0 +1,19 @@
+# local_manifest
+## Description
+Puts repo local manifest into .repo folder for your revision
+
+## Usage
+```bash
+$ ./use_local_manifest.sh
+For what revision are you want to build? (xx,jv,ds)
+manifest changer> xx
+Sony camera choosed
+Please enter directory with Android OS source:
+manifest changer> /home/roman/android
+Putting manifest...
+Done! Please do 'repo sync' in Android source directory for S3 Neo sources download.
+
+# Doing 'repo sync'
+$ cd /home/roman/android
+$ repo sync
+```
---
+++ use_local_manifest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# repo local manifest changer
+#
+
+SCRIPTDIR="$PWD"
+
+echo "For what revision are you want to build? (xx,jv,ds)"
+echo ""
+echo -n "manifest changer> "
+read revision
+case "$revision" in
+xx) echo "Sony camera choosed"
+;;
+jv) echo "Samsung camera choosed"
+;;
+ds) echo "Duos choosed"
+;;
+*)
+;;
+esac
+
+echo "Please enter directory with Android OS source:"
+echo ""
+echo -n "manifest changer> $HOME/"
+read androidsource
+if [ -d "$HOME/$androidsource" ]; then
+echo "Putting manifest..."
+cp $SCRIPTDIR/manifests/main.xml $androidsource/.repo/local_manifests/s3ve3g_main.xml
+cp $SCRIPTDIR/manifests/s3ve3g$revision.xml $androidsource/.repo/local_manifests/s3ve3g$revision.xml
+echo "Done! Please do 'repo sync' in Android source directory for S3 Neo sources download."
+else
+echo "Invalid directory! Abort..." && exit 0
+fi
